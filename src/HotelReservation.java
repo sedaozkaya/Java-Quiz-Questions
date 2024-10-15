603f7ad7cd436cc6e8c9486ad199f88e03e52fe9
Seda <230316033@ogr.cbu.edu.tr>
2024-10-15 19:50:52 +0300

    Hotel reservation quiz completed

---
+++ src/HotelReservation.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class HotelReservation {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.println( "select the days: ");
+        int day= input.nextInt();
+        if (day == 3){
+            System.out.println("enter the number of person: ");
+            int person= input.nextInt();
+            if (person == 1){
+                System.out.println("room type: 3 days " + "\n" + "payment: 900tl");
+            } else if (person == 2){
+                System.out.println("room type: 3 days " + "\n" + "payment: 750tl");
+            } else if (person==3) {
+                System.out.println("room type: 3 days " + "\n" + "payment: 800tl");
+            }
+                else if (person <6) {
+                System.out.println("room type: 3 days " + "\n" + "payment: 2000tl");
+            }
+        } else if (day == 7) {
+            System.out.println("enter the number of person: ");
+            int person= input.nextInt();
+            if (person == 1){
+                System.out.println("room type: 7 days " + "\n" + "payment: 2500tl");
+            } else if (person == 2){
+                System.out.println("room type: 7 days " + "\n" + "payment: 2100tl");
+            } else if (person==3) {
+                System.out.println("room type: 7 days " + "\n" + "payment: 3200tl");
+            }
+            else if (person <6) {
+                System.out.println("NONE");
+            }
+
+        }
+        else {
+            System.out.println("No suitable room found");
+        }
+    }
+}
